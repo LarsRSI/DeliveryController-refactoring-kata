fa070a3a1feefb3a687826cc9f6f1ba1872e7e51
Lars Eckart <lars.eckart@hey.com>
2022-04-07 18:47:36 +0300

    t test about delivery on time

--- src/test/java/kata/DeliveryServiceTest.java
+++ src/test/java/kata/DeliveryServiceTest.java
@@ -15,16 +15,28 @@ import static org.assertj.core.api.Assertions.assertThat;
 @ExtendWith(CommitOnGreenExtension.class)
 class DeliveryServiceTest {
 
+    private DeliveryService deliveryService = new DeliveryService(new NoOpEmailGateway());
+
     @Test
     void marks_delivery_as_arrived() {
-      DeliveryService deliveryService = new DeliveryService(new NoOpEmailGateway());
-      Delivery delivery = new Delivery(123L, "any@example.com", 58.377047f, 26.727889f,
+        Delivery delivery = new Delivery(123L, "any@example.com", 58.377047f, 26.727889f,
               LocalDateTime.of(2022, 4, 7, 18, 28), false, false);
 
       deliveryService.on(new DeliveryEvent(123L, LocalDateTime.of(2022, 4, 7, 18, 28), 58.377047f, 26.727889f),
               List.of(delivery));
 
       assertThat(delivery.isArrived()).isTrue();
+    }
+
+    @Test
+    void marks_delivery_on_time_when_took_less_than_10_minutes() {
+      Delivery delivery = new Delivery(123L, "any@example.com", 58.377047f, 26.727889f,
+              LocalDateTime.of(2022, 4, 7, 18, 28), false, false);
+
+      deliveryService.on(new DeliveryEvent(123L, LocalDateTime.of(2022, 4, 7, 18, 37), 58.377047f, 26.727889f),
+              List.of(delivery));
+
+      assertThat(delivery.isOnTime()).isTrue();
     }
 
     private static class NoOpEmailGateway extends SendgridEmailGateway {
