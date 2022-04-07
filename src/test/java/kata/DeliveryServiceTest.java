2d775d0eb82715acd450295a766c1cb0dd8debe8
Lars Eckart <lars.eckart@hey.com>
2022-04-07 19:40:33 +0300

    t extract methods to create test input

--- src/test/java/kata/DeliveryServiceTest.java
+++ src/test/java/kata/DeliveryServiceTest.java
@@ -19,35 +19,50 @@ class DeliveryServiceTest {
 
     @Test
     void marks_delivery_as_arrived() {
-        Delivery delivery = new Delivery(123L, "any@example.com", 58.377047f, 26.727889f,
-              LocalDateTime.of(2022, 4, 7, 18, 28), false, false);
+        var delivery = createDeliveryWithId(123L);
+        var deliveryEvent = createDeliveryEventWithId(123L);
 
-      deliveryService.on(new DeliveryEvent(123L, LocalDateTime.of(2022, 4, 7, 18, 28), 58.377047f, 26.727889f),
-              List.of(delivery));
+        deliveryService.on(deliveryEvent, List.of(delivery));
 
-      assertThat(delivery.isArrived()).isTrue();
+        assertThat(delivery.isArrived()).isTrue();
+    }
+
+    private DeliveryEvent createDeliveryEventWithId(long id) {
+        return new DeliveryEvent(id, LocalDateTime.of(2022, 4, 7, 18, 28), 58.377047f, 26.727889f);
+    }
+
+    private Delivery createDeliveryWithId(long id) {
+        return new Delivery(id, "any@example.com", 58.377047f, 26.727889f,
+                LocalDateTime.of(2022, 4, 7, 18, 28), false, false);
     }
 
     @Test
     void marks_delivery_on_time_when_took_less_than_10_minutes() {
-      Delivery delivery = new Delivery(123L, "any@example.com", 58.377047f, 26.727889f,
-              LocalDateTime.of(2022, 4, 7, 18, 28), false, false);
+        var delivery = deliveryOrderedAt(10, 40);
+        var deliveryEvent = deliveryEventAt(10, 49);
 
-      deliveryService.on(new DeliveryEvent(123L, LocalDateTime.of(2022, 4, 7, 18, 37), 58.377047f, 26.727889f),
-              List.of(delivery));
+        deliveryService.on(deliveryEvent, List.of(delivery));
 
-      assertThat(delivery.isOnTime()).isTrue();
+        assertThat(delivery.isOnTime()).isTrue();
+    }
+
+    private DeliveryEvent deliveryEventAt(int hour, int minute) {
+        return new DeliveryEvent(123L, LocalDateTime.of(2022, 4, 7, hour, minute), 58.377047f, 26.727889f);
+    }
+
+    private Delivery deliveryOrderedAt(int hour, int minute) {
+        return new Delivery(123L, "any@example.com", 58.377047f, 26.727889f,
+                LocalDateTime.of(2022, 4, 7, hour, minute), false, false);
     }
 
     @Test
     void marks_delivery_not_on_time_when_took_more_or_equal_than_10_minutes() {
-      Delivery delivery = new Delivery(123L, "any@example.com", 58.377047f, 26.727889f,
-              LocalDateTime.of(2022, 4, 7, 18, 28), false, false);
+        var delivery = deliveryOrderedAt(19, 30);
+        var deliveryEvent = deliveryEventAt(19, 40);
 
-      deliveryService.on(new DeliveryEvent(123L, LocalDateTime.of(2022, 4, 7, 18, 38), 58.377047f, 26.727889f),
-              List.of(delivery));
+        deliveryService.on(deliveryEvent, List.of(delivery));
 
-      assertThat(delivery.isOnTime()).isFalse();
+        assertThat(delivery.isOnTime()).isFalse();
     }
 
     private static class NoOpEmailGateway extends SendgridEmailGateway {
